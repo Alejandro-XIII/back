c3898b38eea90afb17ace167e59a0f75c8846864
Alejandro-XIII <78052009+Alejandro-XIII@users.noreply.github.com>
2023-11-12 13:13:22 -0500

    prueba

--- src/main/java/co/udea/ssmu/api/SsmuApiApplication.java
+++ src/main/java/co/udea/ssmu/api/SsmuApiApplication.java
@@ -3,17 +3,10 @@ package co.udea.ssmu.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
-@ComponentScan({
-        "co.udea.ssmu.api.controller",
-        "co.udea.ssmu.api.model",
-        "co.udea.ssmu.api.services",
-        "co.udea.ssmu.api.utils",
-})
 
 public class SsmuApiApplication extends SpringBootServletInitializer {
 
---
+++ src/main/java/co/udea/ssmu/api/dao/IConductorDAO.java
@@ -0,0 +1,14 @@
+package co.udea.ssmu.api.dao;
+
+import co.udea.ssmu.api.model.Conductor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IConductorDAO extends CrudRepository<Conductor,Integer> {
+    @Query("FROM Conductor c WHERE c.promedioCalificacion>4")
+    public List<Conductor> findByPromedioCalificacionGreaterThan();
+}
